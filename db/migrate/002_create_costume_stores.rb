5f7c8f71ac4294e55d9a6fac41feaf65373ddc82
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-02 02:24:23 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,10 +5,10 @@ class CreateCostumeStores < ActiveRecord::Base
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :employees
-      t.boolean :in_business
-      t.datetime :opening
-      t.datetime :closing
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
       t.timestamps
     end
   end
