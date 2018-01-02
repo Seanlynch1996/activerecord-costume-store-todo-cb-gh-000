081f849390f90dba3cb98a3d11905348ca63bfa3
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-02 02:19:02 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Base
+  def change
+    create_table :haunted_houses do |t|
+      t.string  :name
+      t.string  :location
+      t.string  :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.datetime :opening
+      t.datetime :closing
+      t.string  :long_description
+      t.timestamps
+    end
+  end
+end
